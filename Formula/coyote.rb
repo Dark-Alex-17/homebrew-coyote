9c13ee74e10001ac63d74b9907ef1b23702c46dc
Alex Clarke <alex.j.tusa@gmail.com>
2026-05-27 11:00:50 -0600

    feat: renamed Loki to Coyote

--- Formula/coyote.rb
+++ Formula/coyote.rb
@@ -1,23 +1,23 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-class Loki < Formula
+class Coyote < Formula
   desc "All-in-one, batteries included LLM CLI tool"
-  homepage "https://github.com/Dark-Alex-17/loki"
+  homepage "https://github.com/Dark-Alex-17/coyote"
   if OS.mac? and Hardware::CPU.arm?
-    url "https://github.com/Dark-Alex-17/loki/releases/download/v0.4.0/loki-aarch64-apple-darwin.tar.gz"
+    url "https://github.com/Dark-Alex-17/coyote/releases/download/v0.4.0/coyote-aarch64-apple-darwin.tar.gz"
     sha256 "adfbeaca8b50553aa0ebdfe79a549af5ccf7f01451e5d833b109202477234134"
   elsif OS.mac? and Hardware::CPU.intel?
-    url "https://github.com/Dark-Alex-17/loki/releases/download/v0.4.0/loki-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/Dark-Alex-17/coyote/releases/download/v0.4.0/coyote-x86_64-apple-darwin.tar.gz"
     sha256 "47b7a1351d08c2b23ac2c4d601418f129cf03102b8eb833e92a532eaf0f37496"
   else
-    url "https://github.com/Dark-Alex-17/loki/releases/download/v0.4.0/loki-x86_64-unknown-linux-musl.tar.gz"
+    url "https://github.com/Dark-Alex-17/coyote/releases/download/v0.4.0/coyote-x86_64-unknown-linux-musl.tar.gz"
     sha256 "47ea1e4cca8ea9e7a6c70ae2c6078dec9dfd6d9c1330c5c846f65b33733aaeaa"
   end
   version "0.4.0"
   license "MIT"
 
   def install
-    bin.install "loki"
-    ohai "You're done! Get started with \"loki --help\""
+    bin.install "coyote"
+    ohai "You're done! Get started with \"coyote --help\""
   end
 end
--- README.md
+++ README.md
@@ -1,28 +1,29 @@
-# Loki - All-in-one, batteries-included LLM CLI tool
+# Coyote - All-in-one, batteries-included LLM CLI tool
 
 ```
- _          _    _ 
-| |    ___ | | _(_)
-| |   / _ \| |/ / |
-| |__| (_) |   <| |
-|_____\___/|_|\_\_|
+  ____                  _       
+ / ___|___  _   _  ___ | |_ ___ 
+| |   / _ \| | | |/ _ \| __/ _ \
+| |__| (_) | |_| | (_) | ||  __/
+ \____\___/ \__, |\___/ \__\___|
+            |___/               
                    
 ```
 
-`loki` is an all-in-one, batteries-included, LLM CLI tool featuring Shell Assistant, CLI & REPL Mode, RAG, AI Tools & 
+`coyote` is an all-in-one, batteries-included, LLM CLI tool featuring Shell Assistant, CLI & REPL Mode, RAG, AI Tools & 
 Agents, and More.
 
-It is designed to include a number of useful agents, roles, macros, and more so users can get up and running with Loki 
+It is designed to include a number of useful agents, roles, macros, and more so users can get up and running with Coyote 
 in as little time as possible.
 
-**This repo holds the Homebrew Tap and Formula for [Loki](https://github.com/Dark-Alex-17/loki)**
+**This repo holds the Homebrew Tap and Formula for [Coyote](https://github.com/Dark-Alex-17/coyote)**
 
 # Install with
 
 ```
-brew tap Dark-Alex-17/loki
-brew install loki
+brew tap Dark-Alex-17/coyote
+brew install coyote
 
 # If you need to be more specific, use:
-brew install Dark-Alex-17/loki/loki
+brew install Dark-Alex-17/coyote/coyote
 ```
